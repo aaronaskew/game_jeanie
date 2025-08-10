823c07f927c80b1ae9261729112c0757357b95d7
Aaron Askew <aaronaskew@gmail.com>
2025-08-10 18:51:23 -0500

    changed all names to working title Game Genie and game_genie where applicable

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "Bevy game".to_string(), // ToDo
+                        title: "Game Genie".to_string(), // ToDo
                         // Bind to canvas included in `index.html`
                         canvas: Some("#bevy".to_owned()),
                         fit_canvas_to_parent: true,
