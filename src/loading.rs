1772bb818a3906b99eec634fab31f1a2de4f6698
Aaron Askew <aaronaskew@gmail.com>
2025-08-10 22:10:06 -0500

    fixed cargo doc warning

--- src/loading.rs
+++ src/loading.rs
@@ -5,7 +5,7 @@ use bevy_asset_loader::prelude::*;
 
 pub struct LoadingPlugin;
 
-/// This plugin loads all assets using [`AssetLoader`] from a third party bevy plugin
+/// This plugin loads all assets using \[`AssetLoader`\] from a third party bevy plugin
 /// Alternatively you can write the logic to load assets yourself
 /// If interested, take a look at <https://bevy-cheatbook.github.io/features/assets.html>
 impl Plugin for LoadingPlugin {
