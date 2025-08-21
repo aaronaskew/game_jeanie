8ff6099d8b051ac3fdfb9c8ceeec0b34dde4b5a7
Aaron Askew <aaronaskew@gmail.com>
2025-08-21 10:08:54 -0500

    cargo check fmt

--- src/beef_blastoids/ui/mod.rs
+++ src/beef_blastoids/ui/mod.rs
@@ -1,9 +1,9 @@
-mod scoreboard;
 mod game_over;
+mod scoreboard;
 
 use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(scoreboard::plugin)
-    .add_plugins(game_over::plugin);
+        .add_plugins(game_over::plugin);
 }
