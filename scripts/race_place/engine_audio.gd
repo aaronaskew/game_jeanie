c5c12a34471fc9e5a1762f98c18e2c966433e58e
Aaron Askew <aaronaskew@gmail.com>
2025-09-21 12:28:18 -0500

    added engine and tire squeal sounds

---
+++ scripts/race_place/engine_audio.gd
@@ -0,0 +1,14 @@
+class_name EngineAudio
+extends AudioStreamPlayer
+
+@export var min_pitch_scale: float = 0.1
+@export var max_pitch_scale: float = 0.5
+
+@onready var player: Car = %Player
+
+
+func _process(_dt):
+	pitch_scale = (
+		-player.linear_velocity.y / player.max_speed.y * (max_pitch_scale - min_pitch_scale)
+		+ min_pitch_scale
+	)
