56ed6931fe42cecab61aeb190e3e1ba3a848c7ff
Aaron Askew <aaronaskew@gmail.com>
2025-09-21 20:07:49 -0500

    added fuel gauge

--- scripts/race_place/race_place.gd
+++ scripts/race_place/race_place.gd
@@ -5,11 +5,14 @@ extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var player: Car = $Player
+@onready var fuel_gauge: ProgressBar = %FuelGauge
 
 
 func _ready():
 	animation_player.speed_scale = 0
 	animation_player.play("road")
+
+	fuel_gauge.value = 100.0
 
 
 func _process(_delta):
@@ -17,3 +20,9 @@ func _process(_delta):
 	animation_player.speed_scale = (
 		-player.linear_velocity.y / player.max_speed.y * max_roadside_speed_scale
 	)
+
+	update_fuel_gauge()
+
+
+func update_fuel_gauge():
+	fuel_gauge.value = 100.0 - player.distance_traveled * player.fuel_effeciency / player.max_fuel
