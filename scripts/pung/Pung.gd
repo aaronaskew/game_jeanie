21b5c9665fed1a57f2c1235fda0bd40242d7e4a2
Aaron Askew <aaronaskew@gmail.com>
2025-10-12 15:13:59 -0500

    added intro/game over screens to pung

--- scripts/pung/Pung.gd
+++ scripts/pung/Pung.gd
@@ -11,46 +11,70 @@ var pung_root: Pung
 var ball_scene = preload("res://scenes/pung/ball.tscn")
 
 var player_score: int = 0
-var ai_score: int = 0
+var ai_score: int = 9
 
 @onready var player_score_label: Label = %PlayerScore
 @onready var ai_score_label: Label = %AIScore
 @onready var player_paddle = %PlayerPaddle
 @onready var ai_paddle = %AIPaddle
 
+@onready var state_chart: StateChart = $StateChart
+@onready var intro_state: AtomicState = $StateChart/CompoundState/Intro
+@onready var playing_state: AtomicState = $StateChart/CompoundState/Playing
+
+@onready var intro_ui: PanelContainer = $UI/IntroUI
+@onready var game_over_ui: PanelContainer = $UI/GameOverUI
+@onready var game_over_result: Label = game_over_ui.get_node(
+	"MarginContainer/Panel/CenterContainer/VBoxContainer/GameOverResult"
+)
+
 
 func _ready():
 	pung_root = find_parent("Pung")
-	reset_ball(null)
 
 
 func _process(dt: float) -> void:
-	player_score_label.text = str(player_score)
-	ai_score_label.text = str(ai_score)
+	if playing_state.active:
+		player_score_label.text = str(player_score)
+		ai_score_label.text = str(ai_score)
 
-	# Move player
-	if Input.is_action_pressed("ui_up"):
-		player_paddle.move_y(-1)
+		# Move player
+		if Input.is_action_pressed("ui_up"):
+			player_paddle.move_y(-1)
 
-	if Input.is_action_pressed("ui_down"):
-		player_paddle.move_y(1)
+		if Input.is_action_pressed("ui_down"):
+			player_paddle.move_y(1)
 
-	# Move AI
-	var direction: float = ball.position.y - ai_paddle.position.y
-	if direction > ball.speed * dt:
-		ai_paddle.move_y(1)
-	elif direction < -ball.speed * dt:
-		ai_paddle.move_y(-1)
+		# Move AI
+		var direction: float = ball.position.y - ai_paddle.position.y
+		if direction > ball.speed * dt:
+			ai_paddle.move_y(1)
+		elif direction < -ball.speed * dt:
+			ai_paddle.move_y(-1)
 
 
 func _on_ball_ai_scored(ball_node: RigidBody2D) -> void:
 	ai_score += 1
-	reset_ball(ball_node)
+	check_if_game_over()
+	if playing_state.active:
+		reset_ball(ball_node)
 
 
 func _on_ball_player_scored(ball_node: RigidBody2D) -> void:
 	player_score += 1
-	reset_ball(ball_node)
+	check_if_game_over()
+	if playing_state.active:
+		reset_ball(ball_node)
+
+
+func check_if_game_over():
+	if player_score >= 10 || ai_score >= 10:
+		ball.queue_free()
+
+	if player_score >= 10:
+		state_chart.send_event("game_won")
+	elif ai_score >= 10:
+		state_chart.send_event("game_lost")
 
 
 func reset_ball(existing_ball: RigidBody2D):
@@ -63,3 +87,34 @@ func reset_ball(existing_ball: RigidBody2D):
 	ball.ai_scored.connect(_on_ball_ai_scored)
 
 	add_child(ball, true)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if intro_state.active:
+		if event.is_action_pressed("ui_accept"):
+			intro_ui.visible = false
+			state_chart.send_event("intro_finished")
+
+
+func _on_intro_state_entered() -> void:
+	intro_ui.visible = true
+
+
+func _on_intro_state_exited() -> void:
+	intro_ui.visible = false
+
+
+func _on_playing_state_entered() -> void:
+	reset_ball(null)
+
+
+func _on_game_over_child_state_entered() -> void:
+	game_over_ui.visible = true
+
+
+func _on_win_state_entered() -> void:
+	game_over_result.text = "You win!"
+
+
+func _on_lose_state_entered() -> void:
+	game_over_result.text = "You lose"
